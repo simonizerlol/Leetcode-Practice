f7cc7baa956b6c7da6e8daf58e7446142073b4ec
Simon Hsu <simonizerlol@gmail.com>
2020-08-03 13:40:24 +0800

    Create Longest-Common-Prefix.py

---
+++ Longest-Common-Prefix.py
@@ -0,0 +1,34 @@
+# level: easy
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+
+        """
+        if input is empty, return ""
+        find the max length the longest common prefix can be, which is the max length of the shorted string from the input
+        """
+        if len(strs) == 0 :
+            return ""
+
+        '''
+        sorted(strs, key=len) sorts the strings by length, from shortest to longest.
+        '''
+        self.max_length = len(sorted(strs, key = len)[0])
+        self.longest = ""
+
+        '''
+        iterate through index of each input string
+        '''
+        for i in range(self.max_length) :
+            self.char_at_an_index = [word[i] for word in strs]
+            '''
+            count() returns the number of occurrences of a substring in the given string.
+            '''
+            if self.char_at_an_index.count(self.char_at_an_index[0]) == len(self.char_at_an_index) :
+                self.longest += str(self.char_at_an_index[0])
+            else :
+                return self.longest
+        return self.longest
