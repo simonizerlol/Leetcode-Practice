c6532adea58509294a56e39dfe2a2d3baae52e51
Simon Hsu <simonizerlol@gmail.com>
2020-08-07 14:00:08 +0800

    Update Middle-of-the-Linked-List.py

--- Middle-of-the-Linked-List.py
+++ Middle-of-the-Linked-List.py
@@ -1,9 +1,13 @@
 # 876. Middle of the Linked List https://leetcode.com/problems/middle-of-the-linked-list/
 # level: easy
-# complexity:
+# complexity: O(n) time where n is the number of nodes in the given list.
+#             O(1) space, the space used by slow and fast.
 
 # Similar to Leetcode: 21. Merge Two Sorted Lists #
 # Similar to Leetcode: 148. Sort List #
+
+# there are two approach, one is output to array which will result in O(n) space for array size
+# the optimal solution is Fast and Slow Pointer, which is what i have implemented below
 
 # Definition for singly-linked list.
 # class ListNode(object):
